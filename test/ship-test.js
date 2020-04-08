a69dc31c685933fecd23036ed312306ae5c719ad
NataliaColome <nmcolome@gmail.com>
2020-04-08 16:01:06 -0600

    Fix Ship instantiation on test; create Ship

--- test/ship-test.js
+++ test/ship-test.js
@@ -7,38 +7,38 @@ describe('Ship', () => {
   })
 
   it('should have a name', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     assert.equal(cruiser.name, 'Cruiser');
   })
 
   it('should have a length', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     assert.equal(cruiser.length, 3);
   })
 
   it('should have, by default, health points equal to length', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     assert.equal(cruiser.health, 3);
   })
 
   it('should not be sunk by default', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     assert.equal(cruiser.isSunk, false);
   })
 
   it('being hit reduces it\'s health by one', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     cruiser.hit()
     assert.equal(cruiser.health, 2)
   })
 
   it('sinks when it\'s health is reduced to zero', () => {
-    const cruiser = Ship.new("Cruiser", 3);
+    const cruiser = new Ship("Cruiser", 3);
 
     cruiser.hit()
     assert.equal(cruiser.isSunk, false);
