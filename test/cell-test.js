b9c663308a36c606842f81f8ce1fc027da9f2d9b
NataliaColome <nmcolome@gmail.com>
2020-04-08 16:34:01 -0600

    Add tests for rendering a cell

--- test/cell-test.js
+++ test/cell-test.js
@@ -45,4 +45,48 @@ describe('Cell', () => {
     assert.equal(cell.ship.health, 2);
     assert.equal(cell.wasFiredUpon, true);
   })
+
+  it('renders a ”.” if the cell has not been fired upon', () => {
+    const cell = new Cell('B4');
+
+    assert.equal(cell.render(), ".");
+  })
+
+  it('renders an “M” if the cell has been fired upon and it does not contain a ship (the shot was a miss)', () => {
+    const cell = new Cell('B4');
+    cell.fireUpon();
+
+    assert.equal(cell.render(), "M");
+  })
+
+  it('renders an “H” if the cell has been fired upon and it contains a ship (the shot was a hit)', () => {
+    const cell = new Cell('B4');
+    const cruiser = new Ship("Cruiser", 3);
+
+    cell.placeShip(cruiser);
+    cell.fireUpon();
+
+    assert.equal(cell.render(), "H");
+  })
+
+  it('renders an “X” if the cell has been fired upon and its ship has been sunk', () => {
+    const cell = new Cell('B4');
+    const cruiser = new Ship("Cruiser", 3);
+
+    cell.placeShip(cruiser);
+    cell.fireUpon();
+    cell.fireUpon();
+    cell.fireUpon();
+
+    assert.equal(cell.render(), "X");
+  })
+
+  it('renders an “S” if the cell has not been fired upon and contains a ship when we pass a "true" argument to render', () => {
+    const cell = new Cell('B4');
+    const cruiser = new Ship("Cruiser", 3);
+
+    cell.placeShip(cruiser);
+
+    assert.equal(cell.render(true), "S");
+  })
 })
